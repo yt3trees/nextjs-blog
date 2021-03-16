a67539827deb022d93be94105a7232d9761c79bf
yt3trees <y.atforest@gmail.com>
2021-03-16 21:57:51 +0900

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,8 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>yt3treesのプロフィール</p>
+        <p>yt3treesのプロフィール!!!
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
